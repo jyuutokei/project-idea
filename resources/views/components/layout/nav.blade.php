2b15ce717af390fdf4f7179f66b914bce7006514
furantuturu <furantuturu@proton.me>
2026-04-27 21:15:50 +0800

    Add navigation layout component for improved site structure and user access

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,12 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7xl mx-auto h-16 flex justify-between items-center">
+        <div>
+            <a href="/">Idea</a>
+        </div> 
+
+        <div class="flex gap-x-5 items-center">
+            <a href="/login">Login</a>
+            <a href="/register" class="btn">Register</a>
+        </div>
+    </div>
+</nav>
