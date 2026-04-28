567141f0d91a23fe340f448ef99572acefb9c61c
furantuturu <furantuturu@proton.me>
2026-04-28 20:13:34 +0800

    Update routes to redirect home to ideas index and add authenticated access for idea details

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('welcome'));
+Route::redirect('/', '/ideas');
+
+Route::get('/ideas', [IdeaController::class, 'index'])->middleware('auth');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show')->middleware('auth');
 
 Route::get('/register', [RegisteredUserController::class, 'create'])->name('register')->middleware('guest');
 Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
