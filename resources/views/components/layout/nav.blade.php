14d0ad71674c357718d3b01c90a324e31353e289
furantuturu <furantuturu@proton.me>
2026-04-28 15:20:26 +0800

    Implement authentication links in navigation bar with logout form for authenticated users

--- resources/views/components/layout/nav.blade.php
+++ resources/views/components/layout/nav.blade.php
@@ -2,11 +2,22 @@
     <div class="max-w-7xl mx-auto h-16 flex justify-between items-center">
         <div>
             <a href="/">Idea</a>
-        </div> 
+        </div>
 
         <div class="flex gap-x-5 items-center">
+            @auth
+            <form action="/logout" method="POST">
+                @csrf
+                @method('DELETE')
+
+                <button type="submit" class="btn">Logout</button>
+            </form>
+            @endauth
+
+            @guest
             <a href="/login">Login</a>
             <a href="/register" class="btn">Register</a>
+            @endguest
         </div>
     </div>
 </nav>
