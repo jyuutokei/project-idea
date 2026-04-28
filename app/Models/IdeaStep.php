b996de9e53965cb4cf41ea1edcbc35d80a4a3c14
furantuturu <furantuturu@proton.me>
2026-04-28 15:16:28 +0800

    Add property annotations and methods to IdeaStep model for enhanced type hinting and improved code clarity

--- app/Models/IdeaStep.php
+++ app/Models/IdeaStep.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $App\Models\Idea
+ * @property string $description
+ * @property int $completed
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property-read \App\Models\Idea|null $idea
+ * @method static \Database\Factories\IdeaStepFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep whereApp\Models\Idea($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep whereCompleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|IdeaStep whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class IdeaStep extends Model
 {
     /** @use HasFactory<\Database\Factories\IdeaStepFactory> */
