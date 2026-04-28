01b86b1b185942d840090b5e5f0b90bc3f8132fa
furantuturu <furantuturu@proton.me>
2026-04-28 16:49:32 +0800

    Update registration route to include a named route for better clarity and accessibility

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('welcome'));
 
-Route::get('/register', [RegisteredUserController::class, 'create'])->middleware('guest');
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('register')->middleware('guest');
 Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
 
 Route::get('/login', [SessionsController::class, 'create'])->name('login')->middleware('guest');
