4af44056a67e776b1c9be122ee810959a3f83bc8
furantuturu <furantuturu@proton.me>
2026-04-29 20:41:56 +0800

    Refactor status label component to use match expression for class assignment

--- resources/views/components/idea/status-label.blade.php
+++ resources/views/components/idea/status-label.blade.php
@@ -3,17 +3,11 @@
 @php
 $classes = 'inline-block rounded-full border px-2 py-1 text-xs font-medium';
 
-if ($status === 'pending') {
-$classes .= 'bg-yellow-500/10 text-yellow-500 border-yellow-500/20';
-}
-
-if ($status === 'in_progress') {
-$classes .= 'bg-blue-500/10 text-blue-500 border-blue-500/20';
-}
-
-if ($status === 'completed') {
-$classes .= 'bg-primary-500/10 text-primary-500 border-primary-500/20';
-}
+$classes .= match ($status) {
+'in_progress' => ' bg-blue-500/10 text-blue-500 border-blue-500/20',
+'completed' => ' bg-green-500/10 text-green-500 border-green-500/20',
+default => ' bg-yellow-500/10 text-yellow-500 border-yellow-500/20'
+};
 @endphp
 
 <span {{ $attributes(['class' => $classes]) }}>
