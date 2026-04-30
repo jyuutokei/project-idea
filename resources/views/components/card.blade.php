6e63523ffa947c2555aa9adb4654377f8acf14c2
furantuturu <furantuturu@proton.me>
2026-04-30 20:26:14 +0800

    Refactor card component to support dynamic HTML tags based on props

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,13 @@
-<a {{ $attributes(['class' => 'border border-border rounded-lg bg-card md:text-sm p-4']) }}>
+@props(['is' => 'a'])
+
+
+@php
+
+$allowedTags = ['a', 'div', 'section', 'article', 'button', 'span'];
+$tag = in_array($is, $allowedTags, true) ? $is : 'a';
+
+@endphp
+
+<{{ $tag }} {{ $attributes(['class' => 'border border-border rounded-lg bg-card md:text-sm p-4']) }}>
     {{ $slot }}
-</a>
+</{{ $tag }}>
