538f3e4d96287341f327afc4b688ced2b10e17d8
furantuturu <furantuturu@proton.me>
2026-04-30 20:28:01 +0800

    Fix label default value and improve textarea handling in form field component

--- resources/views/components/form/field.blade.php
+++ resources/views/components/form/field.blade.php
@@ -1,11 +1,17 @@
-@props(['label', 'name', 'type' => 'text'])
+@props(['label' => false, 'name', 'type' => 'text'])
 
 <div class="space-y-2">
+    @if ($label)
     <label for="{{ $name }}" class="label">{{ $label }}</label>
+    @endif
+
+    @if ($type === 'textarea')
+    <textarea name="{{ $name }}" id="{{ $name }}"
+        {{ $attributes->merge(['class' => 'textarea']) }}>{{ old($name, '') }}</textarea>
+    @else
     <input type="{{ $type }}" class="input" id="{{ $name }}" name="{{ $name }}"
         value="{{ $type !== 'password' ? old($name, '') : '' }}" {{ $attributes }}>
+    @endif
 
-    @error($name)
-    <p class="error">{{ $message }}</p>
-    @enderror
+    <x-form.error name="{{ $name }}" />
 </div>
