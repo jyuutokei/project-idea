cb81685946eb14ac28bb73cc3bcf374733be2ed3
furantuturu <furantuturu@proton.me>
2026-04-30 20:28:56 +0800

    Add create idea modal with form fields for title, status, and description

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -3,6 +3,11 @@
         <header class="py-8 md:py-12">
             <h1 class="text-3xl font-bold">Ideas</h1>
             <p class="text-muted-background text-sm mt-2">Capture your thoughts. Make a plan</p>
+
+            <x-card x-data @click="$dispatch('open-modal', 'create-idea-modal')" is="button" type="button"
+                class="mt-10 cursor-pointer h-32 w-full text-left">
+                <p>What's the idea?</p>
+            </x-card>
         </header>
 
         <div x-data="{ open: false }" class="relative">
@@ -11,11 +16,9 @@
                 Filter
             </button>
 
-            <div x-show="open" x-transition:enter="transition ease-out duration-300 origin-top-left"
-                x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
-                x-transition:leave="transition ease-in duration-300 origin-top-left"
-                x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-90"
-                class="absolute left-0 ml-20 -mt-9">
+            <div x-show="open" x-transition:enter="ease-out duration-300 origin-top-left"
+                x-transition:enter-start="opacity-0 scale-95" x-transition:leave="ease-in duration-300 origin-top-left"
+                x-transition:leave-end="opacity-0 scale-90" class="absolute left-0 ml-20 -mt-9" style="display: none">
                 @foreach (App\IdeaStatus::cases() as $status)
                 <x-idea.filter-btn status="{{ $status->value }}" count="{{ $statusCount[$status->value] ?? 0 }}" />
                 @endforeach
@@ -43,5 +46,37 @@
                 @endforelse
             </div>
         </div>
+        <x-modal name="create-idea-modal" title="New Idea">
+            <form x-data="{ status: 'pending' }" action="{{ route('idea.store') }}" method="POST">
+                @csrf
+
+                <div class="space-y-6">
+                    <x-form.field label="Title" name="title" placeholder="What is your idea title?" autofocus
+                        required />
+
+                    <div class="space-y-2">
+                        <label for="status" class="label">Status</label>
+
+                        <div class="flex gap-x-3">
+                            @foreach (App\IdeaStatus::cases() as $status)
+                            <button type="button" @click="status = @js($status->value)" class="btn flex-1 h-10"
+                                :class="{ 'btn-outlined': status !== @js($status->value) }">{{ $status->label() }}</button>
+                            @endforeach
+                        </div>
+
+                        <input type="hidden" name="status" id="status" :value="status">
+                        <x-form.error name="status" />
+                    </div>
+
+                    <x-form.field label="Description" name="description" type="textarea"
+                        placeholder="Describe your idea." />
+
+                    <div class="flex justify-end gap-x-5">
+                        <button type="button" class="btn btn-outlined" @click="show = false">Cancel</button>
+                        <button type="submit" class="btn">Create</button>
+                    </div>
+                </div>
+            </form>
+        </x-modal>
     </div>
 </x-layout.layout>
