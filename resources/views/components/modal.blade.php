ce6195a09c950e30f93d0dd16e2fb60f351eb83a
furantuturu <furantuturu@proton.me>
2026-05-01 16:13:29 +0800

    Refactor modal component to improve event handling and maintainability

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,9 +1,10 @@
 @props(['name', 'title'])
 
 <div x-data="{ show: false, name: @js($name) }" x-show="show"
-    @open-modal.window="if($event.detail === name) show = true" @keydown.escape.window="if(show) show = false"
-    x-transition:enter="ease-out duration-200" x-transition:enter-start="opacity-0 -translate-y-4 -translate-x-4"
-    x-transition:leave="ease-in duration-150" x-transition:leave-end="opacity-0 -translate-y-4 -translate-x-4"
+    @open-modal.window="if($event.detail === name) show = true" @close-modal="show = false"
+    @keydown.escape.window="if(show) show = false" x-transition:enter="ease-out duration-200"
+    x-transition:enter-start="opacity-0 -translate-y-4 -translate-x-4" x-transition:leave="ease-in duration-150"
+    x-transition:leave-end="opacity-0 -translate-y-4 -translate-x-4"
     class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-xs" style="display: none"
     tabindex="-1" aria-labelledby="modal-{{ $name }}-title" role="dialog" aria-modal="true">
     <x-card @click.away="show = false" class="shadow-xl max-w-2xl w-full max-h-[80dvh] overflow-auto">
