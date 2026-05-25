7bafd13991b80d2619ec9e675a0c1a5ce896a258
furantuturu <furantuturu@proton.me>
2026-05-25 12:47:09 +0800

    Fix positioning of success message to be fixed instead of absolute

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -24,7 +24,7 @@
 
     @session('success')
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
-        x-transition.opacity.duration.300ms class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
+        x-transition.opacity.duration.300ms class="bg-primary px-4 py-3 fixed bottom-4 right-4 rounded-lg">
         {{ $value }}
     </div>
     @endsession
