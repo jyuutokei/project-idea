66c355dfd64794c28d64444366cba02dd8d602a2
furantuturu <furantuturu@proton.me>
2026-05-25 12:47:32 +0800

    No code changes made in this commit.

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -30,6 +30,13 @@
             <div class="grid md:grid-cols-2 gap-6">
                 @forelse($ideas as $idea)
                 <x-card href="{{ route('idea.show', $idea) }}">
+                    {{-- have to remember to create symbolic link for uploaded image to be available in public folder --}}
+                    @if ($idea->image_path)
+                    <div class="rounded-t-lg overflow-hidden mb-4 -mx-4 -mt-4">
+                        <img src="{{ asset('storage/' . $idea->image_path) }}" alt=""
+                            class="w-full h-auto object-cover">
+                    </div>
+                    @endif
                     <h3 class="text-foreground text-lg">{{ $idea->title }}</h3>
                     <div class="mt-1">
                         <x-idea.status-label status="{{ $idea->status }}">
@@ -46,61 +53,7 @@
                 @endforelse
             </div>
         </div>
-        <x-modal name="create-idea-modal" title="New Idea">
-            <form x-data="{ status: 'pending', newLink: '', links: [] }" action="{{ route('idea.store') }}"
-                method="POST">
-                @csrf
 
-                <div class="space-y-6">
-                    <x-form.field label="Title" name="title" placeholder="What is your idea title?" autofocus
-                        required />
-
-                    <div class="space-y-2">
-                        <label for="status" class="label">Status</label>
-
-                        <div class="flex gap-x-3">
-                            @foreach (App\IdeaStatus::cases() as $status)
-                            <button type="button" @click="status = @js($status->value)" class="btn flex-1 h-10"
-                                :class="{ 'btn-outlined': status !== @js($status->value) }">{{ $status->label() }}</button>
-                            @endforeach
-                        </div>
-
-                        <input type="hidden" name="status" id="status" :value="status">
-                        <x-form.error name="status" />
-                    </div>
-
-                    <x-form.field label="Description" name="description" type="textarea"
-                        placeholder="Describe your idea." />
-
-                    <div>
-                        <fieldset class="space-y-3">
-                            <legend class="label">Links</legend>
-
-                            <template x-for="(link, index) in links" :key="link">
-                                <div class="flex gap-x-2 items-center">
-                                    <input name="links[]" x-model="links[index]" class="input w-full">
-
-                                    <button type="button" @click="links.splice(index, 1)">
-                                        <x-icons.x />
-                                    </button>
-                                </div>
-                            </template>
-
-                            <div class="flex gap-x-2 items-center">
-                                <input x-model="newLink" type="url" id="new-link" autocomplete="url" spellcheck="false"
-                                    placeholder="https://example.com" class="input flex-1">
-                                <button type="button" @click="links.push(newLink.trim()); newLink = ''; "
-                                    :disabled="!newLink.trim()">+</button>
-                            </div>
-                        </fieldset>
-                    </div>
-
-                    <div class="flex justify-end gap-x-5">
-                        <button type="button" class="btn btn-outlined" @click="$dispatch('close-modal')">Cancel</button>
-                        <button type="submit" class="btn">Create</button>
-                    </div>
-                </div>
-            </form>
-        </x-modal>
+        <x-idea.modal />
     </div>
 </x-layout.layout>
