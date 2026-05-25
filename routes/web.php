589256d647f8932ab6eb69f09e4d1aa2bff31952
furantuturu <furantuturu@proton.me>
2026-05-25 12:48:57 +0800

    Refactor routes to include authorization for idea updates and add image and step management routes

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,22 @@ declare(strict_types=1);
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\StepController;
+use App\Http\Controllers\IdeaImageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/ideas');
 
 Route::get('/ideas', [IdeaController::class, 'index'])->name('idea.index')->middleware('auth');
 Route::post('/ideas', [IdeaController::class, 'store'])->name('idea.store')->middleware('auth');
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show')->middleware('auth');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show')->middleware('auth')->can('workWith', 'idea');
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('idea.destroy')->middleware('auth');
+Route::patch('/ideas/{idea}', [IdeaController::class, 'update'])->name('idea.update')->middleware('auth');
+
+Route::delete('/idea/{idea}/image', [IdeaImageController::class, 'destroy'])->name('idea.image.destroy')->middleware('auth');
+
+Route::patch('/steps/{step}', [StepController::class, 'update'])->name('step.update')->middleware('auth');
 
 Route::get('/register', [RegisteredUserController::class, 'create'])->name('register')->middleware('guest');
 Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
@@ -21,3 +29,6 @@ Route::get('/login', [SessionsController::class, 'create'])->name('login')->midd
 Route::post('/login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::delete('/logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')->middleware('auth');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
