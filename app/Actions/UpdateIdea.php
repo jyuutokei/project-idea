c24bdfd39dff6450b091b5cc5ecad788a51de5a6
furantuturu <furantuturu@proton.me>
2026-05-25 12:33:11 +0800

    Add UpdateIdea action class for handling idea updates

---
+++ app/Actions/UpdateIdea.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions;
+
+use App\Models\Idea;
+use Illuminate\Support\Facades\DB;
+
+class UpdateIdea
+{
+    // no need to get current user since it is implied based upon the idea that we are updating
+    public function handle(array $attributes, Idea $idea): void
+    {
+
+        $data = collect($attributes)->only([
+            'title',
+            'description',
+            'status',
+            'links'
+        ])->toArray();
+
+        if ($attributes['image'] ?? false) {
+            $data['image_path'] = $attributes['image']->store('ideas', 'public');
+        }
+
+        DB::transaction(function () use ($idea, $data, $attributes) {
+            $idea->update($data);
+
+            $idea->steps()->delete();
+
+            $idea->steps()->createMany($attributes['steps'] ?? []);
+        });
+
+    }
+}
