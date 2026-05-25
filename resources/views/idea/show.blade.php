5a8c8bc3654b135c4a8ebf0dc75b17062838945a
furantuturu <furantuturu@proton.me>
2026-05-25 12:47:43 +0800

    Enhance idea display with modal edit functionality and improve steps section

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -6,7 +6,8 @@
             </a>
 
             <div class="flex items-center gap-x-3">
-                <button class="btn btn-outlined">Edit Idea</button>
+                <button x-data class="btn btn-outlined" @click="$dispatch('open-modal', 'edit-idea-modal')">Edit
+                    Idea</button>
 
                 <form action="{{ route('idea.destroy', $idea) }}" method="POST">
                     @csrf
@@ -17,6 +18,12 @@
         </div>
 
         <header>
+            {{-- have to remember to create symbolic link for uploaded image to be available in public folder --}}
+            @if ($idea->image_path)
+            <div class="rounded-lg overflow-hidden mb-5">
+                <img src="{{ asset('storage/' . $idea->image_path) }}" alt="" class="w-full h-auto object-cover">
+            </div>
+            @endif
             <h1 class="text-3xl font-bold">{{ $idea->title }}</h1>
             <p class="text-muted-background text-sm mt-2">Created {{ $idea->created_at->diffForHumans() }}</p>
         </header>
@@ -26,8 +33,32 @@
                 {{ $idea->status->label() }}
             </x-idea.status-label>
 
-            <p class="mt-5">{{ $idea->description }}</p>
+            <p class="mt-5">{{ $idea->formattedDescription }}</p>
         </div>
+
+        @if ($idea->steps->count())
+        <div>
+            <h3 class="font-bold text-xl mt-6">Actionable Steps</h3>
+
+            <div class="mt-2 space-y-2">
+                @foreach ($idea->steps as $step)
+                <x-card class="text-primary font-medium flex gap-x-3 items-center">
+                    <form action="{{ route('step.update', $step) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+
+                        <div class="flex items-center gap-x-3">
+                            <button type="submit" role="checkbox"
+                                class="size-5 flex items-center justify-center rounded-lg text-primary-foreground {{ $step->completed ? 'bg-primary' : 'border border-primary' }}">&check;</button>
+                            <span
+                                class="{{ $step->completed ? 'line-through text-muted-foreground' : '' }}">{{ $step->description }}</span>
+                        </div>
+                    </form>
+                </x-card>
+                @endforeach
+            </div>
+        </div>
+        @endif
 
         @if ($idea->links->count())
         <div>
@@ -42,5 +73,7 @@
             </div>
         </div>
         @endif
+
+        <x-idea.modal :idea="$idea" />
     </div>
 </x-layout.layout>
